63603a41ae60f01352aabecf6f154cfc6e874bf6
MykytaDanechkin <nikita.danech@gmail.com>
2024-02-06 16:58:42 +0200

    com

--- src/main/resources/db/changelog/db-changelog-1.1.sql
+++ src/main/resources/db/changelog/db-changelog-1.1.sql
@@ -1,26 +1,29 @@
 --liquibase formatted sql
 
 --changeset mykyda:1
-create table auctions(
+create table funds(
     id bigserial primary key,
     name varchar(50) not null,
-    description varchar(200),
-    photo bytea,
-    author_id bigint not null references users(id),
-    contacts varchar(200) not null,
-    current_bid bigint not null,
-    bidder_id bigint not null references users(id),
-    expire_time timestamp not null,
-    expire_date date not null
+    value float not null
 );
 --changeset mykyda:2
-create table users(
-    id bigserial primary key,
-    name varchar(50) not null
+create table auctions(
+                    id bigserial primary key,
+                    name varchar(50) not null,
+                    description varchar(200),
+                    photo text not null,
+                    author_name bigint not null,
+                    contacts varchar(200) not null,
+                    expire_time timestamp not null,
+                    fund_id bigint not null references funds(id),
+                    fund_stake float not null
 );
 -- changeset mykyda:3
-insert into users(name) values
-                        (
-                            ("testuser1"),(testuser2),(testuser3),(testuser4)
-                        );
+create table bids(
+                     id bigserial primary key,
+                     bidder_name bigint not null,
+                     amount float not null,
+                     auction_id bigint not null references auctions(id)
+);
+
 
