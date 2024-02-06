ac5e90db27bea5871f31c181e256f8b27bf7e4cc
MykytaDanechkin <nikita.danech@gmail.com>
2024-02-06 21:37:32 +0200

    messagecomit

--- src/main/resources/db/changelog/db-changelog-1.1.sql
+++ src/main/resources/db/changelog/db-changelog-1.1.sql
@@ -1,29 +1,11 @@
 --liquibase formatted sql
 
 --changeset mykyda:1
-create table funds(
+create table message (
     id bigserial primary key,
-    name text not null,
-    value float not null
-);
---changeset mykyda:2
-create table auctions(
-                    id bigserial primary key,
-                    name text not null,
-                    description text ,
-                    photo text not null,
-                    author_name bigint not null,
-                    contacts text not null,
-                    expire_time timestamp not null,
-                    fund_id bigint not null references funds(id),
-                    fund_stake float not null
-);
--- changeset mykyda:3
-create table bids(
-                     id bigserial primary key,
-                     bidder_name text not null,
-                     amount float not null,
-                     auction_id bigint not null references auctions(id)
+    message text not null,
+    sender text not null,
+    auction_id bigint not null references auctions(id)
 );
 
 
